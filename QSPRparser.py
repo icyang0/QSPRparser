b40d3e9e95241e4e249a4298830fb333e0420001
icyang0 <icyang0@gmail.com>
2019-01-24 10:03:07 +0800

    now it parses raw file, combines them (IF SAME TESTS), and generate graphs

--- QSPRparser.py
+++ QSPRparser.py
@@ -1,22 +1,31 @@
 # ####todo
 # implement file name selection
 # for the lines selecing specific column, make it not hard coded. ie make it so we can choose whatever column based on the selected paramterers
+# removes the DUT info currently, so actually try and fix this
 
 
 from openpyxl import load_workbook
 from openpyxl import Workbook
-import numpy as np
+from openpyxl.chart import (
+    ScatterChart,
+    Reference,
+    Series,
+)
 
-INPUTFILE1 = 'test-orig1.xlsx'
-OUTPUTFILE1 = 'edited1_test1.xlsx'
-INPUTFILE2 = 'test-orig2.xlsx'
-OUTPUTFILE2 = 'edited1_test2.xlsx'
-COMBINEDFILE = 'edited1_combined.xlsx'
+
+INPUTFILE1 = 'input1.xlsx'
+OUTPUTFILE1 = 'parsed1.xlsx'
+
+INPUTFILE2 = 'input2.xlsx'
+OUTPUTFILE2 = 'parsed2.xlsx'
+
+COMBINEDFILE = 'combined.xlsx'
 
 
 columnTrans = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC')
 #create the tuple of attributes to look for
 keyParametersTX = ('ParameterName', 'channel', 'txChainMask', 'rate', 'powerLevel', 'NumValue')
+keyParametersTXHeader = ('ParameterName', 'channel', 'txChainMask', 'rate', 'target power', 'b1 measured power', 'b1 EVM', 'b2 measured power', 'b2 EVM')
 keyParametersRX = ('ParameterName', 'channel', 'txChainMask', 'rate', 'powerLevel', 'NumValue')
 
 keyParametersTXrow = ('evm', 'avgTxPower')
@@ -28,24 +37,24 @@ def indexNumToColStr(index):
 	return columnTrans[index]
 
 ##############################parses raw data file and saves it#####################
-def parseFile(rawFileName, newSaveFileName):
+def parseFileTX(rawFileName, newSaveFileName):
 
 	#load the file
 	wb = load_workbook(rawFileName)
 	ws = wb.active
-	#remove the first 3 rows
+	#remove the first 3 rows, which includes the DUT info, so maybe change this later
 	ws.delete_rows(0,3)
 
 
-	#find the column index of all the key paramters and creates a tuple with them
+	#find the column index of all the key parameters and creates a tuple with them
 	headerRow = ws[1]
 	keyParameterIndexTup = ()
 	for i in range(len(keyParametersTX)):
 		for x in range(len(headerRow)):
-			if (keyParametersTX[i] in headerRow[x].value):
+			if (keyParametersTX[i] == headerRow[x].value):
 				dog = (x,)
 				keyParameterIndexTup = keyParameterIndexTup + dog
-	
+				
 	#go through and delete the columns that are not in the key parameters			
 	i = len(keyParameterIndexTup) - 1
 	for z in range( len(headerRow) -1, -1, -1):
@@ -118,8 +127,9 @@ def parseFile(rawFileName, newSaveFileName):
 	return 1	
 
 
-##########################combines two files####################
-def combineParsedFile(file1, file2, combinedFileName):
+##################combines two files for non-similar test conditions####################
+#########################NOT FINISHED###################
+def combineNSParsedFile(file1, file2, combinedFileName):
 	#load the file
 	wb1 = load_workbook(file1)
 	ws1 = wb1.active
@@ -131,7 +141,6 @@ def combineParsedFile(file1, file2, combinedFileName):
 	#load the test conditoins into memory for the 1st file 
 #	testCondsTup1 = ws1[('B') + ':' + ('D')]
 	testConds1 = ws1[('B') + ':' + ('E')]
-#	testConds1 = (np.array(list(testCondsTup1))).T
 	
 	#load the test conditoins into memory for the 2nd file 
 	testConds2 = ws2[('B') + ':' + ('E')]
@@ -178,14 +187,91 @@ def combineParsedFile(file1, file2, combinedFileName):
 	#save the file!	
 	wb1.save(combinedFileName)	
 	
-	return 1
 	
+	
+##################combines two files for SAME test conditions####################
+def combineSParsedFileTX(file1, file2, combinedFileName):
+	#load the file
+	wb1 = load_workbook(file1)
+	ws1 = wb1.active
+	
+	#load the file
+	wb2 = load_workbook(file2)
+	ws2 = wb2.active
+	
+	#add two new columns after the last ones to add more data
+	ws1.insert_cols( len(keyParametersTX) + 2,2)
+	
+	#load up the 2nd parsed file's power and EVM
+	mPowerCol2 = ws2['F']
+	mEVMCol2 = ws2['G']
+	
+	#add the data from the 2nd workbook to the first
+	for x in range(len(mPowerCol2)):
+		ws1['H' + (str(x+1))] = mPowerCol2[x].value
+		ws1['I' + (str(x+1))] = mEVMCol2[x].value
 		
 		
+	#add in the row explaining what each column is
+	ws1.insert_rows(1)
+	for i in range(len(keyParametersTXHeader)):
+		ws1[indexNumToColStr(i) + '1'] = keyParametersTXHeader[i]
+		
+	# #save the file!	
+	wb1.save(combinedFileName)	
 
-#parseFile(INPUTFILE1, OUTPUTFILE1)
-#parseFile(INPUTFILE2, OUTPUTFILE2)
+##################adds all the charts yo####################
+def addChartsParsedTX(fileName, outputFileName):	
+	#then load it up to work with
+	wbc = load_workbook(fileName)
+	wsc = wbc.active
 	
-combineParsedFile(OUTPUTFILE1, OUTPUTFILE2, COMBINEDFILE)	
+	tPowerCol = wsc['E']
+	
+	#loop through the saved data and index where there's a change in power level	
+	newPowerIndexList = []
+	newPowerIndexList.append(2)
+	
+	#for irow in range(1, len(tPowerCol)):
+	for irow in range(2, len(tPowerCol)):
+		prevVal = tPowerCol[irow-1].value
+		if (prevVal > tPowerCol[irow].value):
+			newPowerIndexList.append(irow+1)
+			
+	for i in range(len(newPowerIndexList)):
+		chart = ScatterChart()
+		chart.style = 12
+		chart.height = 3
+		
+		minxrow = newPowerIndexList[i]
+		minyrow = newPowerIndexList[i]
+		
+		if (i == (len(newPowerIndexList) - 1)):
+			maxrow = len(tPowerCol)
+		else:
+			maxrow = newPowerIndexList[i+1] - 1
+
+		xValues1 = Reference(wsc, min_col=6, min_row=minxrow, max_row=maxrow)
+		xValues2 = Reference(wsc, min_col=8, min_row=minxrow, max_row=maxrow)
+	
+		yValues1 = Reference(wsc, min_col=7, min_row=minyrow, max_row=maxrow)
+		yValues2 = Reference(wsc, min_col=9, min_row=minyrow, max_row=maxrow)
+		series1 = Series(yValues1, xValues1, title='b1 evm')
+		series2 = Series(yValues2, xValues2, title='b2 evm')
+		
+		chart.series.append(series1)
+		chart.series.append(series2)
+		wsc.add_chart(chart, ("J"+str(newPowerIndexList[i])))
+		
+	
+	#save file
+	wbc.save(outputFileName)
+	
 	
 
+#parseFileTX(INPUTFILE1, OUTPUTFILE1)
+#parseFileTX(INPUTFILE2, OUTPUTFILE2)
+	
+combineSParsedFileTX(OUTPUTFILE1, OUTPUTFILE2, 'test.xlsx')	
+	
+addChartsParsedTX('test.xlsx', 'scatter.xlsx')
